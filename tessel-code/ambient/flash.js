786a91ba81290eacb8a862648282232ce128e402
Kia Fathi <kiafathi@me.com>
2014-07-19 11:51:16 -0700

    Remove extra ambient files

--- tessel-code/ambient/flash.js
+++
@@ -1,4 +0,0 @@
-var tessel = require('tessel');
-var ambientLib = require('ambient-attx4');
-
-ambientLib.updateFirmware( tessel.port['A'], 'node_modules/ambient-attx4/firmware/src/ambient-attx4.hex');
